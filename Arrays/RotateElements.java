6babb114e9e3aa24eaf519bd5a773c944ccf2c28
Khushi Agarwal <magickhu@gmail.com>
2024-02-29 17:11:14 +0530

    Create RotateElements.java

---
+++ Arrays/RotateElements.java
@@ -0,0 +1,33 @@
+public class Main {
+  // Function to Reverse the array
+  public static void Reverse(int[] arr, int start, int end) {
+    while (start <= end) {
+      int temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  // Function to Rotate k elements to right
+  public static void Rotateeletoright(int[] arr, int n, int k) {
+    // Reverse first n-k elements
+    Reverse(arr, 0, n - k - 1);
+    // Reverse last k elements
+    Reverse(arr, n - k, n - 1);
+    // Reverse whole array
+    Reverse(arr, 0, n - 1);
+  }
+  // Function to Rotate k elements to left
+void Rotateeletoleft(int arr[], int n, int k)
+{
+  // Reverse first k elements
+  Reverse(arr, 0, k - 1);
+  // Reverse last n-k elements
+  Reverse(arr, k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+//Time Complexity – O(N) where N is the number of elements in an array
+
+Space Complexity – O(1) since no extra space is required
